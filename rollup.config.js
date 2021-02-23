71a79a21483f964b01241562b5c39e2e84d9f7bf
Ryan <rzimmerman@talusanalytics.com>
2021-02-23 11:43:16 -0700

    Adding live reloading

--- rollup.config.js
+++ rollup.config.js
@@ -46,7 +46,8 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+		livereload()
 	],
 	watch: {
 		clearScreen: false
