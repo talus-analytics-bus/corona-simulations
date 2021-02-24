867dfe6a59fa768e1e920d20b3d7a005f5a3f79c
Ryan <rzimmerman@talusanalytics.com>
2021-02-24 12:27:40 -0700

    Ha, looks like they already had a better way to toggle livereload on and off in here

--- rollup.config.js
+++ rollup.config.js
@@ -42,12 +42,10 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		// !production && livereload('public'),
+		!production && livereload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser(),
-		// livereload()
 	],
 	watch: {
 		clearScreen: false
