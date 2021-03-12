3a28c1d16d5cd2ff752c5767f5bdf7f6f9eb8231
Ryan <rzimmerman@talusanalytics.com>
2021-03-11 22:58:31 -0700

    Adding export model run link and export parameters link

--- src/models/talusSEIRModel/params.js
+++ src/models/talusSEIRModel/params.js
@@ -1,81 +1,76 @@
-
 const getEpiParams = model_run => {
+  const eP = {}
 
-    const eP = {}
+  eP.N = model_run.population
 
-    eP.N = model_run.population
+  eP.fraction_critical =
+    (1 - model_run.percent_asymp) *
+    model_run.hospitalization_rate *
+    model_run.hospitalized_cases_requiring_icu_care
 
-    eP.fraction_critical = (
-        (1 - model_run.percent_asymp)
-        * model_run.hospitalization_rate
-        * model_run.hospitalized_cases_requiring_icu_care
-    )
+  eP.alpha = 1 / model_run.presymptomatic_period
 
-    eP.alpha = 1 / model_run.presymptomatic_period
+  eP.beta_A = model_run.beta_asymp / eP.N
 
-    eP.beta_A = model_run.beta_asymp / eP.N
+  eP.beta = [
+    0,
+    model_run.beta_mild / eP.N,
+    model_run.beta_hospitalized / eP.N,
+    model_run.beta_icu / eP.N,
+    // TODO move beta.A to model params
+    eP.beta_A,
+  ]
 
-    eP.beta = [
-        0,
-        model_run.beta_mild / eP.N,
-        model_run.beta_hospitalized / eP.N,
-        model_run.beta_icu / eP.N,
-        // TODO move beta.A to model params
-        eP.beta_A,
-    ]
+  eP.beta_mild = model_run.beta_mild
+  eP.beta_asymp = model_run.beta_asymp
 
-    eP.beta_mild = model_run.beta_mild
-    eP.beta_asymp = model_run.beta_asymp
+  // have to calculate these in order and then put them into arrays
+  eP.gamma_0 = 0
+  eP.gamma_1 =
+    (1 / model_run.duration_mild_infections) *
+    (1 - model_run.hospitalization_rate)
 
-    // have to calculate these in order and then put them into arrays
-    eP.gamma_0 = 0
-    eP.gamma_1 = (1 / model_run.duration_mild_infections) * (1 - model_run.hospitalization_rate)
+  eP.rho_0 = 0
+  eP.rho_1 = 1 / model_run.duration_mild_infections - eP.gamma_1
+  eP.rho_2 =
+    (1 / model_run.hospital_time_recovery) *
+    (eP.fraction_critical / model_run.hospitalization_rate)
 
-    eP.rho_0 = 0
-    eP.rho_1 = (1 / model_run.duration_mild_infections) - eP.gamma_1
-    eP.rho_2 = (1 / model_run.hospital_time_recovery) * (eP.fraction_critical / model_run.hospitalization_rate)
+  eP.gamma_2 = 1 / model_run.hospital_time_recovery - eP.rho_2
 
-    eP.gamma_2 = (1 / model_run.hospital_time_recovery) - eP.rho_2
+  eP.mu =
+    (1 / model_run.icu_time_death) *
+    (model_run.case_fatality_rate / eP.fraction_critical)
 
-    eP.mu = (1 / model_run.icu_time_death) * (model_run.case_fatality_rate / eP.fraction_critical)
+  eP.gamma_3 = 1 / model_run.icu_time_death - eP.mu
 
-    eP.gamma_3 = (1 / model_run.icu_time_death) - eP.mu
+  eP.gamma_A = 1 / model_run.duration_asymp_infections
 
-    eP.gamma_A = 1 / model_run.duration_asymp_infections
+  eP.gamma = [eP.gamma_0, eP.gamma_1, eP.gamma_2, eP.gamma_3, eP.gamma_A]
 
-    eP.gamma = [
-        eP.gamma_0,
-        eP.gamma_1,
-        eP.gamma_2,
-        eP.gamma_3,
-        eP.gamma_A,
-    ]
+  // "gamma": L(gamma_0, gamma_1, gamma_2, gamma_3, A = 0),
+  eP.rho = [eP.rho_0, eP.rho_1, eP.rho_2]
 
-    // "gamma": L(gamma_0, gamma_1, gamma_2, gamma_3, A = 0),
-    eP.rho = [
-        eP.rho_0,
-        eP.rho_1,
-        eP.rho_2
-    ]
+  eP.f = model_run.percent_asymp
 
-    eP.f = model_run.percent_asymp
+  // these are used to get the growth-rate-base R estimation
+  // Serial interval =[Incubation Period]+1/2[Duration of mild infection]
+  eP.serial_interval =
+    model_run.presymptomatic_period + model_run.duration_mild_infections / 2
 
-    // these are used to get the growth-rate-base R estimation
-    // Serial interval =[Incubation Period]+1/2[Duration of mild infection]
-    eP.serial_interval = model_run.presymptomatic_period + (model_run.duration_mild_infections / 2)
+  // f = Ratio of mean infectious period to mean serial interval
+  eP.f_ratio = model_run.duration_mild_infections / 2 / eP.serial_interval
 
-    // f = Ratio of mean infectious period to mean serial interval
-    eP.f_ratio = (model_run.duration_mild_infections / 2) / eP.serial_interval
+  // r0, given initial conditions and epi parameters
+  eP.r0 =
+    model_run.population *
+    (eP.beta[1] / (eP.rho[1] + eP.gamma[1]) +
+      (eP.rho[1] / (eP.rho[1] + eP.gamma[1])) *
+        (eP.beta[2] / (eP.rho[2] + eP.gamma[2]) +
+          (eP.rho[2] / (eP.rho[2] + eP.gamma[2])) *
+            (eP.beta[3] / (eP.mu + eP.gamma[3]))))
 
-    // r0, given initial conditions and epi parameters
-    eP.r0 = model_run.population * (
-        ((eP.beta[1]) / (eP.rho[1] + eP.gamma[1]))
-        + (eP.rho[1] / (eP.rho[1] + eP.gamma[1]))
-        * (eP.beta[2] / (eP.rho[2] + eP.gamma[2])
-            + (eP.rho[2] / (eP.rho[2] + eP.gamma[2]))
-            * (eP.beta[3] / (eP.mu + eP.gamma[3]))))
-
-    return eP
+  return eP
 }
 
 export default getEpiParams
