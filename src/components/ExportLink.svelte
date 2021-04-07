2242d6a1e51740a83db8cf09cdb06808330680b1
Ryan <rzimmerman@talusanalytics.com>
2021-04-06 18:12:17 -0600

    Just to be on the safe side, always building the CSV

--- src/components/ExportLink.svelte
+++ src/components/ExportLink.svelte
@@ -1,31 +1,35 @@
 <script>
+import { afterUpdate } from 'svelte'
 
 export let P_all
 
 let csvString = 'data:text/csv;charset=utf-8,'
 
-Object.keys(P_all[0]).forEach(key => {
-    csvString += key ==='rt' ? 'r_0,' : key + ','
-});
+afterUpdate(() => {
+    csvString = 'data:text/csv;charset=utf-8,'
 
-csvString += 'total symptomatic, total infected'
-csvString += '\r\n'
-
-P_all.forEach(day => {
-    Object.values(day).forEach((value, index) => {
-        if (index === 7) {
-            csvString += value.toFixed(2) + ','
-        } else {
-            csvString += Math.round(value) + ','
-        }
+    Object.keys(P_all[0]).forEach(key => {
+        csvString += key ==='rt' ? 'r_0,' : key + ','
     });
 
-    const symptomatic = day.infected + day.hospitalized + day.icu
-    csvString += Math.round(symptomatic) + ','
-    csvString += Math.round(symptomatic + day.asymptomatic)
+    csvString += 'total symptomatic, total infected'
     csvString += '\r\n'
-})
 
+    P_all.forEach(day => {
+        Object.values(day).forEach((value, index) => {
+            if (index === 7) {
+                csvString += value.toFixed(2) + ','
+            } else {
+                csvString += Math.round(value) + ','
+            }
+        });
+
+        const symptomatic = day.infected + day.hospitalized + day.icu
+        csvString += Math.round(symptomatic) + ','
+        csvString += Math.round(symptomatic + day.asymptomatic)
+        csvString += '\r\n'
+    })
+})
 </script>
 
 <a class="downloadModel" href={encodeURI(csvString)} download="Model Output.csv">Download Model Run</a>
